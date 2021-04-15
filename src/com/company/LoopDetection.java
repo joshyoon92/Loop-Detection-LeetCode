c432ebffc055cc66ab6390e1cba64ee72517b5b3
Joshua Yoon <joshyoon92@gmail.com>
2021-04-15 18:00:46 +0900

    Initial commit

---
+++ src/com/company/LoopDetection.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class LoopDetection extends ListNode {
+    LoopDetection(int x) {
+        super(x);
+    }
+    public ListNode FindBeginnnig(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                break;
+            }
+        }
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return fast;
+    }
+}
